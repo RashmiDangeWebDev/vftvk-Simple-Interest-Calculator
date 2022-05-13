53bf877e62d6ff363cd4b0c85b719c217dad3b15
Rashmi Dange <rashmi.dange@gmail.com>
2022-05-13 07:40:42 +0000

    changes made from theia lab environment - updated files

--- script.js
+++ script.js
@@ -6,21 +6,25 @@ function compute()
     {
         alert("Enter Positive Number");
         document.getElementById("principal").focus();
+        // clearing the text before calculating each time
+        document.getElementById("result").innerHTML=" ";
     } 
     else
     {
+        // clearing the text before calculating each time
+        document.getElementById("result").innerHTML=" ";
         //if valid principal amount calculate intrest and display on html page
         var rate = document.getElementById("rate").value;
         var years = document.getElementById("years").value;
         var interest = principal * years * rate /100;
         var year = new Date().getFullYear()+parseInt(years);
-        document.getElementById("result").innerHTML="If you deposite \<b>"+principal+",\</b>\<br\> at an interest rate of \<b>"+
-        rate+"%\</b>,\<br\> you will receive amount of \<b>"+interest+"\</b>,\<br\> in the year \<b>"+year+"\</b>\<br\>"
+        document.getElementById("result").innerHTML="If you deposite \<mark>\<b>"+principal+",\</mark>\</b>\<br\> at an interest rate of \<mark>\<b>"+
+        rate+"%\</mark>\</b>,\<br\> you will receive amount of \<mark>\<b>"+interest+"\</mark>\</b>,\<br\> in the year \<mark>\<b>"+year+"\</mark>\</b>\<br\>"
     }
 }
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("rate_val").innerText=rateval+"%";
 }
         
